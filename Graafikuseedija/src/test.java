970eab4cdea6f3f3d2c679dd2728c2b4af1e1b39
Enri-Taar <56914042+Enri-Taar@users.noreply.github.com>
2020-03-15 15:59:10 +0200

    first println part 3

--- Graafikuseedija/src/test.java
+++ Graafikuseedija/src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello there");
         System.out.println("general kenobi!");
+        System.out.println("suema");
     }
 }
